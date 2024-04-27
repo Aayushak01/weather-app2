7fd8109ce312d72f6c118e4ee2a66175d89c1c66
Aayushak01 <121002949+Aayushak01@users.noreply.github.com>
2024-04-27 16:18:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,64 +1,4 @@
-// console.log("Aayush");
-// const API_key = "1f9a7b2ed33e2581572203b3e6269a8d";
 
-// function renderWeatherInfo(data) {
-//     let newPara = document.createElement('p');
-//     newPara.textContent = `${data?.main?.temp.toFixed(2)} C`;
-//     document.body.appendChild(newPara);
-// }
-
-// async function showWeather(){
-
-//     try {
-        
-//         let city_name = "Goa";
-
-//         const response =await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${API_key}`);
-
-//         const data = await response.json();
-//         console.log("Weather data => " ,data)
-
-//         renderWeatherInfo(data);
-//     } catch (error) {
-        
-//     }
-    
-// }
-//  async function getCustomWeather(){
-//     try {
-//         let lon = 15.6333;
-//         let lat =  18.3333;
-        
-//         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}`);
-//         const data = await response.json();
-
-//         console.log(data);
-       
-//         renderWeatherInfo(data);
-//     } catch (error) {
-//         console.log("Error Found " , err);
-//     }
-//  }
-
-//  function getLocation(){
-//     try {
-//         if(navigator.geolocation){
-//             navigator.geolocation.getCurrentPosition(showPosition);
-//         }else {
-//             console.log("No geolocation Support");
-//         }
-//     } catch (error) {
-        
-//     }
-//  }
-
-//  function showPosition(position){
-//     let lat = position.coords.latitude;
-//     let lon = position.coords.longitude;
-
-//     console.log(lat);
-//     console.log(lon);
-//  }
 const userTab = document.querySelector("[data-userWeather]");
 const searchTab = document.querySelector("[data-searchWeather]");
 const userContainer = document.querySelector(".weather-container");
@@ -169,7 +109,7 @@ function renderWeatherInfo(weatherInfo) {
     countryIcon.src = `https://flagcdn.com/144x108/${weatherInfo?.sys?.country.toLowerCase()}.png`;
     desc.innerText = weatherInfo?.weather?.[0]?.description;
     weatherIcon.src = `http://openweathermap.org/img/w/${weatherInfo?.weather?.[0]?.icon}.png`;
-    temp.innerText = `${weatherInfo?.main?.temp} °F`;
+    temp.innerText = `${(weatherInfo?.main?.temp - 273.15).toFixed(2)} °C`;
     windspeed.innerText = `${weatherInfo?.wind?.speed} m/s`;
     humidity.innerText = `${weatherInfo?.main?.humidity}%`;
     cloudiness.innerText = `${weatherInfo?.clouds?.all}%`;
